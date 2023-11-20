8139c4d849fd9b961eadd55928229d6b6412cc8c
Rabboni <rabboni171@gmail.com>
2023-11-20 15:19:50 +0500

    fix some mistakes

--- for/for_12/main.go
+++ for/for_12/main.go
@@ -6,8 +6,8 @@ func main() {
 	var n int
 	fmt.Scan(&n)
 	multiple := 1.0
-	for i := 1.1; i <= float64(n); i++ {
-		multiple *= i
+	for i := 1; i <= n; i++ {
+		multiple *= 1.0 + (float64(i) / 10.0)
 
 	}
 	fmt.Println(multiple)
